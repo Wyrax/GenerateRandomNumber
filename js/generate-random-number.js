37f2f444c639fddd4c5006b735bbbdd6aeea7d49
Wyrax <wyrax@ukr.net>
2020-12-17 10:44:47 +0200

    ggit: Updated GenerateRandomNumber

--- js/generate-random-number.js
+++ js/generate-random-number.js
@@ -2,33 +2,29 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('%c >>> DOM loaded ', 'background: #222; color: #fcee21');
 
     let randomButton = document.getElementById('random_button');
+    let randomNumberDiv = document.getElementById('random_number');
+    let winNumberSpan = document.getElementById('win_number');
 
     randomButton.addEventListener('click', () => {
+
         let minValue = document.getElementById('from').value;
         let maxValue = document.getElementById('upto').value;
-        // console.log({minValue});
-        // console.log({maxValue});
-        // console.log('cliCK');
-        // console.log(randomIntFromInterval(minValue, maxValue));
 
-        // var array = new Uint32Array(10);
-        // window.crypto.getRandomValues(array);
-
-        // console.log("Your lucky numbers:");
-        // for (var i = 0; i < array.length; i++) {
-        // console.log(array[i]);
-        // }
-
-        // var random = new Random.Random();
-        // alert("Random value from 1 to 100: " + random.integer(1, 100));
+        winNumberSpan.textContent = '';
+        randomNumberDiv.style.backgroundImage = 'url("/img/fireworks.gif?' + new Date().getTime() + '")';
 
         let random = new Random(Random.engines.mt19937().autoSeed());
-        console.log('Random value from ' + minValue + ' to ' + maxValue+ ': ' + random.integer(minValue, maxValue));
+        let winNumber = random.integer(minValue, maxValue);
+        console.log('Random value from ' + minValue + ' to ' + maxValue+ ': ' + winNumber);
+        // randomNumberDiv.classList.add('fireworks');
+        setTimeout(function() { winNumberSpan.textContent = winNumber; }, 500);
+        setTimeout(function() {
+            // randomNumberDiv.classList.remove('fireworks');
+            randomNumberDiv.style.backgroundImage = 'none';
+        }, 5500);
+
     });
 
-    function randomIntFromInterval(min, max) { // min and max included 
-        console.log({min});
-        console.log({max});
-        return Math.floor(Math.random() * (max - min + 1) + min);
-      }
 });
+
+/* background-image: url('/img/fireworks.gif'); */
